0c7f92a51e298cd6d94c91922d1083c03912a69e
Itai Zaretsky <itai.zaretsky@gmail.com>
2023-04-17 18:14:05 +0300

    need to check electron mobility

--- question3.py
+++ question3.py
@@ -3,21 +3,48 @@ import matplotlib.pyplot as plt
 from scipy import constants as const
 from sklearn import datasets
 import sympy as sp
-
 k = const.Boltzmann * 6.25e18
-def calc_energy():
+e = const.elementary_charge
+m_0 = const.electron_mass
+h = const.h
+def calc_energy(t):
 
     temperature = [1/(15+273), 1/(30+273), 1/(80+273), 1/(110+273), 1/(125+273), 1/(160+273), 1/(175+273), 1/(195+273)]
     resistance = [7180, 3170, 345, 120, 75, 28.5, 20, 12.5]
     resistance = [sp.ln(r) for r in resistance]
     plt.plot(temperature, resistance)
-    a = (resistance[3] - resistance[1])/(temperature[3] - temperature[1])
-    slope = (resistance[3] - resistance[1])/(temperature[3] - temperature[1])
-    print(slope)
-    e_g = -slope*2*k/(sp.ln(1100/(sp.sqrt(2.09e37))))
-    print(e_g)
+    plt.show()
+    slope = (resistance[1] - resistance[3])/(temperature[3] - temperature[1])
+    e_g = slope*2*k/(sp.ln(1100/(sp.sqrt(2.09e37))))
+    print("Eg =", round(float(e_g), 3), "[eV]")
+    calc_mobility(t, slope, float(e_g))
 
 
-calc_energy()
+def calc_mobility(t, slope, e_g):
+    nc = 2*((2*np.pi*0.37*m_0*1.38e-23*t)/(h**2))**(3/2)
+    nc_t = nc/100**3
+    nv = 2*((2*np.pi*0.18*m_0*1.38e-23*t)/(h**2))**(3/2)
+    nv_t = nv/100**3
+    ni_t = np.sqrt(nc_t*nv_t)*np.exp(-e_g/(2*k*t))
+    mu_p = 1100*(t/300)**(-3/2)
+    sl = float(slope)
+    rho = sp.exp(-sl/t)
+    sigma = 1/rho
+    mu_n = (sigma-(e*ni_t*mu_p))/(e*ni_t)
+    print("The electron mobility at the given temperature is:", round(float(mu_n), 3), "[cm^2/Vs]")
 
+
+def menu():
+    while 1:
+        print("Enter Temperature in [K]:")
+        t = int(input())
+        if t < 0:
+            print("Try Again! Temperature must be positive.")
+        else:
+            break
+    calc_energy(t)
+
+
+
+menu()
 
